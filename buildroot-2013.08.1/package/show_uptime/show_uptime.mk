febc38b15ae127a9f2a414b387332a192bedecf0
JeGa <jega@linux>
2013-11-16 20:23:45 +0100

    Added package show_uptime. Does not work yet.

---
+++ buildroot-2013.08.1/package/show_uptime/show_uptime.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# show_uptime
+#
+################################################################################
+
+SHOW_UPTIME_VERSION = 1.0
+SHOW_UPTIME_SOURCE = show_uptime.tar.gz
+SHOW_UPTIME_SITE_METHOD = file
+SHOW_UPTIME_SITE = ~/systemarm/ARMBuildroot/application/
+
+define SHOW_UPTIME_BUILD_CMDS
+  $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+endef
+
+define SHOW_UPTIME_INSTALL_TARGET_CMDS
+  $(INSTALL) -D -m 0755 $(@D)/show_uptime_* $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
