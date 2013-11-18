f46e920328af0ded4b5546226c5b814f0e3709b3
Fabian Meyer <rookfighter@develop>
2013-11-18 18:54:45 +0100

    Still not wokring...

--- buildroot-2013.08.1/package/jefa_web/jefa_web.mk
+++ buildroot-2013.08.1/package/jefa_web/jefa_web.mk
@@ -1,14 +1,10 @@
-JEFA_WEB_VERSION = 1.
+JEFA_WEB_VERSION = 1.0
 JEFA_WEB_SOURCE = jefa_web.tar.gz
 JEFA_WEB_SITE_METHOD = file
-JEFA_WEB_SITE = ~/systemarm/ARMBuildroot/application
-
-define SHOW_UPTIME_BUILD_CMDS
-  
-endef
+JEFA_WEB_SITE = ~/systemarm/ARMBuildroot/website
 
 define SHOW_UPTIME_INSTALL_TARGET_CMDS
-  $(INSTALL) -D -m 0755 $(@D)/* $(TARGET_DIR)/www
+  $(INSTALL)-D -m 0755 $(@D)/index.html $(TARGET_DIR)/index.html
 endef
 
 $(eval $(generic-package))
