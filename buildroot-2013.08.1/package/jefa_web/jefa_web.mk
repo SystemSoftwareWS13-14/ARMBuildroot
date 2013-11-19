ab96bd45a88c03da8fe774965020f1228e6aa207
Fabian Meyer <rookfighter@develop>
2013-11-19 08:41:46 +0100

    Index.html now works!

--- buildroot-2013.08.1/package/jefa_web/jefa_web.mk
+++ buildroot-2013.08.1/package/jefa_web/jefa_web.mk
@@ -10,7 +10,8 @@ JEFA_WEB_SITE_METHOD = file
 JEFA_WEB_SITE = ~/systemarm/ARMBuildroot/website
 
 define JEFA_WEB_INSTALL_TARGET_CMDS
-  $(INSTALL) -d -m 0755 $(@D)/* $(TARGETDIR)/www
+  $(INSTALL) -D -m 0755 $(@D)/index.html $(TARGET_DIR)/www/index.html
+  $(INSTALL) -D -d -m 0755 $(@D)/cgi-bin $(TARGET_DIR)/www/cgi-bin
 endef
 
 $(eval $(generic-package))
