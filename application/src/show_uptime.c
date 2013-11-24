b93f86e9b2b2ad098694c9e27a8d78edd5e179b9
JeGa <jega@linux>
2013-11-24 14:16:09 +0100

    Removed tarballs from git repo and added sleep to uptime script.

--- application/src/show_uptime.c
+++ application/src/show_uptime.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <sys/sysinfo.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
 	struct sysinfo info;
-	sysinfo(&info);
 
-	printf("=====================\n");
-	printf("Uptime = %ld\n", info.uptime);
-	printf("=====================\n");
+	while (1) {
+		sysinfo(&info);
+
+		printf("=====================\n");
+		printf("Uptime = %ld\n", info.uptime);
+		printf("=====================\n");
+	
+		sleep(1);
+	}
 
 	return 0;
 }
