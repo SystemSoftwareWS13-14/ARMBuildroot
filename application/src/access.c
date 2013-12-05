68be6844399190601f03bfd8f9d5ddac2a7c0980
Fabian Meyer <rookfighter@develop>
2013-12-05 23:41:57 +0100

    Access works now.

--- application/src/access.c
+++ application/src/access.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define TRIES 10
+#define DEF_TRIES 10
 
-int main(int argc, char *argv[])
+int main(int argc, const char *argv[])
 {
-	int i, opened;
-	char *device;
-	FILE* opened_files[TRIES];
+	int i, opened, tries;
+	const char *device;
+	FILE **opened_files;
 
 	if(argc < 1)
 	{
-		printf("Give an argument.\n");
+		printf("Usage: access [filename] [trycount]\n");
 		return -1;
 	}
 
-	device = strcat("/dev/", argv[1]);
+	if(argc > 2)
+		tries = atoi(argv[2]);
+	else
+		tries = DEF_TRIES;
+	
+	printf("To open: %s.\n", argv[1]);
+	
+	opened_files = malloc(tries * sizeof (FILE*));
+	if(opened_files == NULL)
+		return -2;
+
+	device = argv[1];
 	opened = 0;
 
-	for(i = 0; i < TRIES; ++i)
+	for(i = 0; i < tries; i++)
 	{
-		printf("%d Opening \"%s\"... ", i, device);
+		printf("%d Opening %s... ", i, device);
 		opened_files[i] = fopen(device, "r");
 		if(opened_files[i] == NULL)
 			printf("FAILED\n");
@@ -31,16 +42,17 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	for(i = 0; i < TRIES; ++i)
+	for(i = 0; i < tries; i++)
 	{
 		if(opened_files[i] == NULL)
 			continue;
-		printf("%d Closing \"%s\"... ",i, device);
+		printf("%d Closing %s... ",i, device);
 		if(fclose(opened_files[i]) < 0)
 			printf("FAILED\n");
 		else
 			printf("SUCCESS\n");
 	}
+	free(opened_files);
 	printf("%d times opened.", opened);
 	return 0;
 }
